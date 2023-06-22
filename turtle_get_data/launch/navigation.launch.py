f64f1cb244376e460e2090444a06b06ab3f02174
tiago369 <tiagobarreto581@gmail.com>
2023-06-22 08:59:58 -0300

    Add navigation

---
+++ turtle_get_data/launch/navigation.launch.py
@@ -0,0 +1,57 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
+
+
+def generate_launch_description():
+    pkg_dir = get_package_share_directory('turtle_get_data')
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    map_dir = LaunchConfiguration(
+        'map',
+        default=os.path.join(
+            pkg_dir,
+            'config',
+            'arena1.yaml'))
+
+    param_dir = LaunchConfiguration(
+        'params_file',
+        default=os.path.join(
+            pkg_dir,
+            'config',
+            'dwb.yaml'))
+
+    nav2_launch_file_dir = os.path.join(get_package_share_directory('nav2_bringup'), 'launch')
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'map',
+            default_value=map_dir,
+            description='Full path to map file to load'),
+
+        DeclareLaunchArgument(
+            'params_file',
+            default_value=param_dir,
+            description='Full path to param file to load'),
+
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([nav2_launch_file_dir, '/bringup_launch.py']),
+            launch_arguments={
+                'map': map_dir,
+                'use_sim_time': use_sim_time,
+                'params_file': param_dir}.items(),
+        ),
+    ])
